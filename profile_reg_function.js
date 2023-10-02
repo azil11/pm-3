2fc1930ed821cb63be9563cae2fd03e35f00f5f7
azil11 <146702682+azil11@users.noreply.github.com>
2023-10-02 13:51:42 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function profile(namaDepan, namaBelakang, email) {
+  console.log("Nama : " + namaDepan + ""+ namaBelakang)
+  console.log("Email : " + email)
+}
+
+profile("Azriel", "Zulham", "azrielzulhamzahir21@gmail.com")
